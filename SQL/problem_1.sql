219f5615c2dba92f053e95fceddc4c23fd83728b
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:16:23 +0100

    SQL - Revising the Select Query I. [Easy]

---
+++ SQL/problem_1.sql
@@ -0,0 +1,7 @@
+/*
+	SQL Exercise:
+		Name: Revising the Select Query I. [Easy]
+		Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+*/
+
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE='USA';
