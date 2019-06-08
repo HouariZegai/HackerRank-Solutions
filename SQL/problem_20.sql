bd0c7212e24a9ea1428623dfbf41be0dde7f9bed
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:36:15 +0100

    SQL - Employee Salaries. [Easy]

---
+++ SQL/problem_20.sql
@@ -0,0 +1,9 @@
+/*
+	SQL Exercise:
+		Name: Employee Salaries. [Easy]
+		Link: https://www.hackerrank.com/challenges/salary-of-employees/problem
+*/
+
+SELECT name 
+	FROM Employee 
+	WHERE salary > 2000 AND months < 10;
