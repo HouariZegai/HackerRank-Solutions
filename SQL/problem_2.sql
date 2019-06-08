f6120e7ed36dd5925cc4dddb5f7389520cc3674f
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:18:14 +0100

    SQL - Revising the Select Query II. [Easy]

---
+++ SQL/problem_2.sql
@@ -0,0 +1,7 @@
+/*
+	SQL Exercise:
+		Name: Revising the Select Query II. [Easy]
+		Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+*/
+
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = "USA"
