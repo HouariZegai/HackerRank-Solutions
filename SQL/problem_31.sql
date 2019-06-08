dceb9bfa0e6455bcc7fc80977a43fc4929266135
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:53:07 +0100

    SQL - Population Density Difference. [Easy]

---
+++ SQL/problem_31.sql
@@ -0,0 +1,8 @@
+/*
+	SQL Exercise:
+		Name: Population Density Difference. [Easy]
+		Link: https://www.hackerrank.com/challenges/population-density-difference/problem
+*/
+
+SELECT MAX(POPULATION) - MIN(POPULATION) 
+	FROM CITY;
