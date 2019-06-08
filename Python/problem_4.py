158a3817145a6bd275cb6a1d05a4ced16cf60e18
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:02:06 +0100

    Python - Python: Division. [Easy]

---
+++ Python/problem_4.py
@@ -0,0 +1,14 @@
+"""
+
+Exercise: 
+	Name: Python: Division. [Easy]
+	Link: https://www.hackerrank.com/challenges/python-division/problem
+
+"""
+
+from __future__ import division
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a//b)
+    print(a/b)
