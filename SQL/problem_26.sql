cb3a88f865cf5cfac13d672cd0c484a05318ced9
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:38:44 +0100

    SQL - Revising Aggregations - The Count Function. [Easy]

---
+++ SQL/problem_26.sql
@@ -0,0 +1,9 @@
+/*
+	SQL Exercise:
+		Name: Revising Aggregations - The Count Function. [Easy]
+		Link: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+*/
+
+SELECT COUNT(*) 
+	FROM CITY 
+	WHERE POPULATION > 100000;
