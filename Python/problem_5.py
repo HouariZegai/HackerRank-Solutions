b45a73a0c1d395361ac6938e4f0014b65f32b3ae
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:04:46 +0100

    Python - Loops. [Easy]

---
+++ Python/problem_5.py
@@ -0,0 +1,12 @@
+"""
+
+Exercise: 
+	Name: Loops. [Easy]
+	Link: https://www.hackerrank.com/challenges/python-loops/problem
+
+"""
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(n):
+        print(i*i)
