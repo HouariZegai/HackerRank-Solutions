8c1a481df84c24b087d9804afdcdef5b2fe55ec0
HouariZegai <houarizegai14@gmail.com>
2019-06-08 10:55:32 +0100

    Python - Say "Hello, World!" With Python [Easy]

---
+++ Python/problem_1.py
@@ -0,0 +1,10 @@
+"""
+
+Exercise: 
+	Name: Say "Hello, World!" With Python. [Easy]
+	Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+
+"""
+
+if __name__ == '__main__':
+    print "Hello, World!"
