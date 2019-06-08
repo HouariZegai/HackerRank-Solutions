36f30286a9ac809f1917dcec1a6684ec4645a4dc
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:23:20 +0100

    SQL - Japanese Cities' Attributes. [Easy]

---
+++ SQL/problem_5.sql
@@ -0,0 +1,9 @@
+/*
+	SQL Exercise:
+		Name: Japanese Cities' Attributes. [Easy]
+		Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+*/
+
+SELECT * 
+	FROM CITY 
+	WHERE COUNTRYCODE="JPN";
