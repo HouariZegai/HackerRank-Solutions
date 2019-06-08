007bb9ce71589dcb9b750a9a6217b237fa904df3
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:26:35 +0100

    SQL - Weather Observation Station 1. [Easy]

---
+++ SQL/problem_7.sql
@@ -0,0 +1,8 @@
+/*
+	SQL Exercise:
+		Name: Weather Observation Station 1. [Easy]
+		Link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+*/
+
+SELECT CITY, STATE 
+	FROM STATION;
