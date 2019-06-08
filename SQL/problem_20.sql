2a53aa117a1b819f7f491a1753a2fc863a061839
HouariZegai <houarizegai14@gmail.com>
2019-06-08 13:48:09 +0100

    Refactoring structure of folders

--- SQL/problem_20.sql
+++
@@ -1,9 +0,0 @@
-/*
-	SQL Exercise:
-		Name: Employee Salaries. [Easy]
-		Link: https://www.hackerrank.com/challenges/salary-of-employees/problem
-*/
-
-SELECT name 
-	FROM Employee 
-	WHERE salary > 2000 AND months < 10;
