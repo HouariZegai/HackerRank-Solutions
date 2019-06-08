6e6400c15dfaadc1e1173b2285f44f6ca8552ece
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:24:59 +0100

    SQL - Japanese Cities' Names. [Easy]

---
+++ SQL/problem_6.sql
@@ -0,0 +1,9 @@
+/*
+	SQL Exercise:
+		Name: Japanese Cities' Names. [Easy]
+		Link: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+*/
+
+SELECT NAME 
+	FROM CITY 
+	WHERE COUNTRYCODE="JPN";
