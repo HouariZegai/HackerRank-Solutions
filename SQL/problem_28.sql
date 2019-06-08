94bee0b161ec49d5b38fa09e75278a8f38c78858
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:41:50 +0100

    SQL - Revising Aggregations - Averages. [Easy]

---
+++ SQL/problem_28.sql
@@ -0,0 +1,9 @@
+/*
+	SQL Exercise:
+		Name: Revising Aggregations - Averages. [Easy]
+		Link: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+*/
+
+SELECT AVG(POPULATION) 
+	FROM CITY 
+	WHERE DISTRICT = 'California';
