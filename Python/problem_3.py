8d0b7d01c83698e782c6ea9a55271442b3a7e0a2
HouariZegai <houarizegai14@gmail.com>
2019-06-08 11:00:19 +0100

    Python - Arithmetic Operators. [Easy]

---
+++ Python/problem_3.py
@@ -0,0 +1,15 @@
+"""
+
+Exercise: 
+	Name: Arithmetic Operators. [Easy]
+	Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+"""
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a+b)
+
+print(a-b)
+print(a*b)
